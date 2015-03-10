af78012c8aaa9510a6606c5e6ec19cc4c4578a00
Hiran <gondim.game@gmail.com>
2015-03-10 09:42:28 -0300

    Conversores
    Declaração das funções e variaveis

--- BibliotecaCalculoNumerico/Conversores/decbin.h
+++ BibliotecaCalculoNumerico/Conversores/decbin.h
@@ -1,10 +1,22 @@
 #ifndef DECBIN_H
 #define DECBIN_H
 
+#include <stdio.h>
+#include <math.h>
+
 class DecBin
 {
+    double numero,frac,inteira;
+
+    char *nlnt;
+
+    int *nums;
+
 public:
     DecBin();
+    DecBin(int Valor);
+    DecBin(double Valor);
+    ~DecBin();
 };
 
 #endif // DECBIN_H
